3110b0a40e6ef6f7e3d6ec1820bb329fc769b013
Ismail <ismayili662@gmail.com>
2023-07-07 14:23:27 +0400

    update

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,6 +2,8 @@ import "../../ui/layout/mainLayout";
 import "../../ui/components/sidebar";
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 import "../../ui/pages/home/home";
+import '../../ui/layout/auth/authLayout'
+
 FlowRouter.route("/", {
   name: "App.home",
   action() {
@@ -10,12 +12,24 @@ FlowRouter.route("/", {
     });
   },
 });
+
+
 import "../../ui/pages/dashboard/dashboard";
 FlowRouter.route("/dashboard", {
   name: "App.dashboard",
   action() {
     BlazeLayout.render("mainLayout", {
       main: "dashboard",
+    });
+  },
+});
+
+import '../../ui/pages/auth/login'
+FlowRouter.route("/login", {
+  name: "App.login",
+  action() {
+    BlazeLayout.render("authLayout", {
+      main: "login",
     });
   },
 });
